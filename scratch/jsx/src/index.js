c1900c39f9815b1e7f17f2662b23a29b48ec1298
Gautam Gupta <ggupta78@gmail.com>
2021-03-22 17:06:52 +0530

    Complete JSX section

--- scratch/jsx/src/index.js
+++ scratch/jsx/src/index.js
@@ -2,9 +2,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+  return 'Click On Me!';
+}
+
 // Create a react component
 const App = function () {
-  return <div>Hi there!</div>;
+  //const buttonText = 'Click Me!';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {getButtonText()}
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
