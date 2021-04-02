c357dd4dbd9571090e8ffd62577bdb6d1f840da8
Gautam Gupta <ggupta78@gmail.com>
2021-04-02 14:25:13 +0530

    Complete Season app

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './SeasonDisplay.css';
 
 const getSeason = (lat, month) => {
   console.log(lat, month);
@@ -19,10 +20,10 @@ const SeasonDisplay = (props) => {
   const { text, iconName } = seasonConfig[season];
 
   return (
-    <div>
-      <i className={`${iconName} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${iconName} icon`} />
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
   );
 };
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
   state = { lat: null, errorMessage: '' };
@@ -12,12 +13,7 @@ class App extends React.Component {
     );
   }
 
-  // componentDidUpdate() {
-  //   console.log('ComponentDidUpdate()');
-  // }
-
-  render() {
-    //console.log('Inside render()');
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -26,7 +22,13 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading....</div>;
+    return (
+      <Spinner message="Acquiring Geolocation. Please accept location request." />
+    );
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
