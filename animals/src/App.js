8d9b9bc84fd5fa0ea11f79b6bf0f4e82bcc31324
Gautam Gupta <ggupta78@gmail.com>
2024-08-10 11:32:43 +0530

    add random animal

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cow", "cat", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
-  return <div>List of Animals!</div>;
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  return (
+    <div>
+      <h1>List of Animals!</h1>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>Animals: {animals}</div>
+    </div>
+  );
 }
 
 export default App;
