803ea887a3d4b242825d84e74150d4939837f82f
Gautam Gupta <ggupta78@gmail.com>
2024-09-04 14:15:58 +0530

    books app contd

--- books/src/App.js
+++ books/src/App.js
@@ -1,15 +1,42 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 import { getByTitle } from "@testing-library/react";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {
-    console.log("title", title);
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
   };
 
-  return <BookCreate onCreate={createBook} />;
+  const createBook = (title) => {
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title },
+    ];
+    setBooks(updatedBooks);
+    // console.dir(books);
+  };
+
+  const bookList = () => {
+    let bookTitles = "";
+    books.forEach((book) => {
+      bookTitles += `${book.title}<br />`;
+    });
+    return bookTitles;
+  };
+
+  return (
+    <div className="app">
+      <BookList books={books} onDelete={deleteBookById} />
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
