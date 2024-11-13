8f89648c1aa6c3fb47e86ed2154daf1cc6f4faf7
Gautam Gupta <ggupta78@gmail.com>
2024-11-13 13:33:42 +0530

    add comps project

---
+++ comps/src/App.js
@@ -0,0 +1,24 @@
+import Button from "./Button";
+function App() {
+  return (
+    <div>
+      <div>
+        <Button primary>Hello World!</Button>
+      </div>
+      <div>
+        <Button secondary>Hi there!</Button>
+      </div>
+      <div>
+        <Button>Bye!</Button>
+      </div>
+      <div>
+        <Button>See Ya!</Button>
+      </div>
+      <div>
+        <Button>Hello again!</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
