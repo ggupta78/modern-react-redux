171ae87cc13632480109dfad04fabc69978d3aee
Gautam Gupta <ggupta78@gmail.com>
2024-11-17 18:58:26 +0530

    complete first section for comps - custom navigation and routing systems

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,17 +1,30 @@
+import { MdAddChart, MdAdd, MdAddAlarm } from "react-icons/md";
 import Button from "./Button";
+
 function App() {
+  const handleClick = () => {
+    console.log("Clicked!!");
+  };
+
   return (
     <div>
       <div>
-        <Button primary outline rounded>
+        <Button primary outline rounded className="mb-5" onClick={handleClick}>
+          <MdAddChart />
           Hello World!
         </Button>
       </div>
       <div>
-        <Button secondary>Hi there!</Button>
+        <Button secondary onMouseOver={handleClick}>
+          <MdAdd />
+          Hi there!
+        </Button>
       </div>
       <div>
-        <Button success>Bye!</Button>
+        <Button success>
+          <MdAddAlarm />
+          Bye!
+        </Button>
       </div>
       <div>
         <Button warning outline>
