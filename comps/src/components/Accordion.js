ec5459584edcd3bc4f1af0d9dfa6b8f999902968
Gautam Gupta <ggupta78@gmail.com>
2024-11-17 21:18:56 +0530

    add icons

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -1,14 +1,24 @@
 import { useState } from "react";
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
 
 function Accordian({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
 
+  const handleClick = (index) => {
+    setExpandedIndex(index);
+  };
+
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
+    const icon = isExpanded ? <MdKeyboardArrowDown /> : <MdKeyboardArrowUp />;
+
     return (
       <div key={item.id}>
-        <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+        <div onClick={() => handleClick(index)}>
+          {icon}
+          {item.label}
+        </div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
