b76bcb1f21629c73fd6a8261adb79e495ecc6a1c
Gautam Gupta <ggupta78@gmail.com>
2024-11-17 20:58:17 +0530

    setup basic accordion

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,43 +1,28 @@
-import { MdAddChart, MdAdd, MdAddAlarm } from "react-icons/md";
-import Button from "./Button";
+import Accordian from "./components/Accordion";
 
 function App() {
-  const handleClick = () => {
-    console.log("Clicked!!");
-  };
+  const items = [
+    {
+      id: "fdsasdf",
+      label: "You can use React in a project.",
+      content:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos nobis, nihil provident aliquid ducimus asperiores animi maxime, est sit neque beatae recusandae blanditiis, error inventore odit voluptas nulla dicta magni.",
+    },
+    {
+      id: "fdsasdf2",
+      label: "You can use Javascript in a project.",
+      content:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos nobis, nihil provident aliquid ducimus asperiores animi maxime, est sit neque beatae recusandae blanditiis, error inventore odit voluptas nulla dicta magni.",
+    },
+    {
+      id: "fdsasdf3",
+      label: "You can use CSS in a project.",
+      content:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos nobis, nihil provident aliquid ducimus asperiores animi maxime, est sit neque beatae recusandae blanditiis, error inventore odit voluptas nulla dicta magni.",
+    },
+  ];
 
-  return (
-    <div>
-      <div>
-        <Button primary outline rounded className="mb-5" onClick={handleClick}>
-          <MdAddChart />
-          Hello World!
-        </Button>
-      </div>
-      <div>
-        <Button secondary onMouseOver={handleClick}>
-          <MdAdd />
-          Hi there!
-        </Button>
-      </div>
-      <div>
-        <Button success>
-          <MdAddAlarm />
-          Bye!
-        </Button>
-      </div>
-      <div>
-        <Button warning outline>
-          See Ya!
-        </Button>
-      </div>
-      <div>
-        <Button danger rounded>
-          Hello again!
-        </Button>
-      </div>
-    </div>
-  );
+  return <Accordian items={items} />;
 }
 
 export default App;
---
+++ comps/src/pages/ButtonPage.js
@@ -0,0 +1,43 @@
+import { MdAddChart, MdAdd, MdAddAlarm } from "react-icons/md";
+import Button from "../components/Button";
+
+function ButtonPage() {
+  const handleClick = () => {
+    console.log("Clicked!!");
+  };
+
+  return (
+    <div>
+      <div>
+        <Button primary outline rounded className="mb-5" onClick={handleClick}>
+          <MdAddChart />
+          Hello World!
+        </Button>
+      </div>
+      <div>
+        <Button secondary onMouseOver={handleClick}>
+          <MdAdd />
+          Hi there!
+        </Button>
+      </div>
+      <div>
+        <Button success>
+          <MdAddAlarm />
+          Bye!
+        </Button>
+      </div>
+      <div>
+        <Button warning outline>
+          See Ya!
+        </Button>
+      </div>
+      <div>
+        <Button danger rounded>
+          Hello again!
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonPage;
