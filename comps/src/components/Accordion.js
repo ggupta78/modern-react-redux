e4f70430574e4bb97b080bb3851d726e0f03673f
Gautam Gupta <ggupta78@gmail.com>
2024-11-18 19:35:18 +0530

    complete Mastering the State Design Pattern

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -2,29 +2,48 @@ import { useState } from "react";
 import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
 
 function Accordian({ items }) {
-  const [expandedIndex, setExpandedIndex] = useState(0);
+  const [expandedIndex, setExpandedIndex] = useState(-1);
 
-  const handleClick = (index) => {
-    setExpandedIndex(index);
+  const handleClick = (nextIndex) => {
+    setExpandedIndex((currentExpandedIndex) => {
+      if (currentExpandedIndex === nextIndex) {
+        return -1;
+      } else {
+        return nextIndex;
+      }
+    });
+
+    // if (expandedIndex === nextIndex) {
+    //   setExpandedIndex(-1);
+    // } else {
+    //   setExpandedIndex(nextIndex);
+    // }
   };
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
-    const icon = isExpanded ? <MdKeyboardArrowDown /> : <MdKeyboardArrowUp />;
+    const icon = (
+      <span className="text-2xl">
+        {isExpanded ? <MdKeyboardArrowDown /> : <MdKeyboardArrowUp />}
+      </span>
+    );
 
     return (
       <div key={item.id}>
-        <div onClick={() => handleClick(index)}>
-          {icon}
+        <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
           {item.label}
+          {icon}
         </div>
-        {isExpanded && <div>{item.content}</div>}
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
 
-  return <div>{renderedItems}</div>;
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
 
 export default Accordian;
